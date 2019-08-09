6e5f4c8c5039c4e5839fb0448a5d96da92edb150
Rachel Wong <rachelwong.ly@gmail.com>
2019-08-09 19:32:25 -0400

    initial commit

---
+++ server/index.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const PORT = 5000;
+
+app.get('/', (req, res) => {
+  res.send('hello world');
+});
+
+
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
+});
