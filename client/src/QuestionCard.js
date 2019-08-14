30f7d896996b4ae53a196fdf2960d532d2882bc0
Rachel Wong <rachelwong.ly@gmail.com>
2019-08-14 12:19:57 -0400

    added category to form data collection

--- client/src/QuestionCard.js
+++ client/src/QuestionCard.js
@@ -5,7 +5,11 @@ function QuestionCard({ question, modifyAnswers }) {
   const updateInput = e => {
     console.log(e.target.name);
     console.log(e.target.value);
-    const newAnswer = { question_id: parseInt(e.target.name.slice(6)), agree_rating: parseInt(e.target.value) }
+    console.log(e.target.getAttribute('category'));
+    const newAnswer = { 
+      question_id: parseInt(e.target.name.slice(6)),
+      agree_rating: parseInt(e.target.value),
+      category: e.target.getAttribute('category') }
     modifyAnswers(newAnswer);
   }
 
@@ -14,13 +18,13 @@ function QuestionCard({ question, modifyAnswers }) {
       <p>{question.text}</p>
       <div className="answerbox" onChange={updateInput}>
       Disagree&ensp;
-        <input className="dots" type="radio" name={'answer' + question.id} value="1"/>
-        <input className="dots" type="radio" name={'answer' + question.id} value="2"/>
-        <input className="dots" type="radio" name={'answer' + question.id} value="3"/>
-        <input className="dots" type="radio" name={'answer' + question.id} value="4"/>
-        <input className="dots" type="radio" name={'answer' + question.id} value="5"/>
-        <input className="dots" type="radio" name={'answer' + question.id} value="6"/>
-        <input className="dots" type="radio" name={'answer' + question.id} value="7"/>
+        <input className="dots" type="radio" name={'answer' + question.id} category={question.category} value="1"/>
+        <input className="dots" type="radio" name={'answer' + question.id} category={question.category} value="2"/>
+        <input className="dots" type="radio" name={'answer' + question.id} category={question.category} value="3"/>
+        <input className="dots" type="radio" name={'answer' + question.id} category={question.category} value="4"/>
+        <input className="dots" type="radio" name={'answer' + question.id} category={question.category} value="5"/>
+        <input className="dots" type="radio" name={'answer' + question.id} category={question.category} value="6"/>
+        <input className="dots" type="radio" name={'answer' + question.id} category={question.category} value="7"/>
         &emsp;Agree
       </div>
     </div>
