e5265d146900efe9240a924380865d5bc0387a69
Rachel Wong <rachelwong.ly@gmail.com>
2019-08-14 18:08:45 -0400

    completed styling of perspective page

--- client/src/ResultsPage.js
+++ client/src/ResultsPage.js
@@ -3,42 +3,42 @@ import React from 'react';
 function ResultsPage({ user }) {
 
   return (
-    <div className="ResultsPage">
+    <div id="resultspage">
 
-      <h1>{user.email}</h1>
-      <div>
+      <div className='left'>
+        <h3>{user.email}</h3>
         <h1>Your Perspective</h1>
-        <h6>Your perspective is {user.perspective_result}</h6>
+        <h3>Your perspective is {user.perspective_result}</h3>
       </div>
 
-      <div>
+      <div className='right'>
 
         <div className='perspective'>
-          <span className='inline'>Introversion(I)</span>
+          <span className='inline'>Introversion (I)</span>
           <div className={user.perspective_result[0] === 'I' ? 'coloured bar inline' : 'bar inline'}></div>
           <div className={user.perspective_result[0] === 'E' ? 'coloured bar inline' : 'bar inline'}></div>
-          <span className='inline'>Extraversion(E)</span>
+          <span className='inline'>Extraversion (E)</span>
         </div>
 
         <div className='perspective'>
-          <span className='inline'>Sensing(S)</span>
+          <span className='inline'>Sensing (S)</span>
           <div className={user.perspective_result[1] === 'S' ? 'coloured bar inline' : 'bar inline'}></div>
           <div className={user.perspective_result[1] === 'N' ? 'coloured bar inline' : 'bar inline'}></div>
-          <span className='inline'>Intuition(N)</span>
+          <span className='inline'>Intuition (N)</span>
         </div>
 
         <div className='perspective'>
-          <span className='inline'>Thinking(T)</span>
+          <span className='inline'>Thinking (T)</span>
           <div className={user.perspective_result[2] === 'T' ? 'coloured bar inline' : 'bar inline'}></div>
           <div className={user.perspective_result[2] === 'F' ? 'coloured bar inline' : 'bar inline'}></div>
-          <span className='inline'>Feeling(F)</span>
+          <span className='inline'>Feeling (F)</span>
         </div>
         
         <div className='perspective'>
-          <span className='inline'>Judging(J)</span>
+          <span className='inline'>Judging (J)</span>
           <div className={user.perspective_result[3] === 'J' ? 'coloured bar inline' : 'bar inline'}></div>
           <div className={user.perspective_result[3] === 'P' ? 'coloured bar inline' : 'bar inline'}></div>
-          <span className='inline'>Perceiving(P)</span>
+          <span className='inline'>Perceiving (P)</span>
         </div>
 
       </div>
